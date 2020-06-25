8d24f1ba50fd56a99fa80b105e248e530fc017b9
Aleksander Waage <aleksander.waage@tv2.no>
2020-06-25 14:35:01 +0200

    Initial Commit

---
+++ Bitcoin Price Tracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bitcoin Price Tracker
+//
+//  Created by Aleksander Waage on 25/06/2020.
+//  Copyright © 2020 Waageweb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
